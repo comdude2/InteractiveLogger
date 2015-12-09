b5c6ae2aac7d54d1185b6e4441d3766b0bbc9722
Matt <comdude2@msn.com>
2015-12-09 00:59:20 +0000

    Push

--- src/net/comdude2/plugins/interactivelogger/playback/RecordingPlayer.java
+++ src/net/comdude2/plugins/interactivelogger/playback/RecordingPlayer.java
@@ -1,9 +1,19 @@
 package net.comdude2.plugins.interactivelogger.playback;
 
+import net.comdude2.plugins.interactivelogger.main.InteractiveLogger;
+import net.comdude2.plugins.interactivelogger.recording.Recording;
+
 public class RecordingPlayer {
+	
+	private InteractiveLogger il = null;
+	private Recording recording = null;
 	
 	public RecordingPlayer(){
 		
+	}
+	
+	public void load(Recording recording){
+		this.recording = recording;
 	}
 	
 	public void play(){
--- src/net/comdude2/plugins/interactivelogger/recording/Recorder.java
+++ src/net/comdude2/plugins/interactivelogger/recording/Recorder.java
@@ -1,5 +1,7 @@
 package net.comdude2.plugins.interactivelogger.recording;
 
 public class Recorder {
-
+	
+	
+	
 }
